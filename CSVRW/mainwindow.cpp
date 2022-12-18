659bfa7deffa32839b550193b6e4e25482d660c1
whyme111 <838771417@qq.com>
2022-12-18 18:01:50 +0800

    使用Qtablemodel，对csv文件进行读取操作

---
+++ CSVRW/mainwindow.cpp
@@ -0,0 +1,47 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "QTextStream"
+#include "qfile.h"
+#include "qfiledialog.h"
+#include "csvtablemodel.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->tableView->setEditTriggers(QAbstractItemView::DoubleClicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btn_load_clicked()
+{
+    const QString fileName =
+            QFileDialog::getOpenFileName(this, tr("Choose a data file"), "", "*.csv");
+    if (fileName.isEmpty())
+        return;
+    if(model)
+    {
+        delete model;
+        model =nullptr;
+    }
+    model = new CSVTableModel(fileName,',', true, ui->tableView);
+    ui->tableView->setModel(model);
+}
+
+void MainWindow::on_btn_saveas_clicked()
+{
+    const QString fileName = QFileDialog::getSaveFileName(this, tr("Choose a data file"), "", "*.csv");
+    if (fileName.isEmpty())
+        return;
+    if(model)
+        model->save(fileName);
+
+}
+
---
+++ CSVRW/mainwindow.h
@@ -0,0 +1,30 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class CSVTableModel;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btn_load_clicked();
+
+    void on_btn_saveas_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    CSVTableModel* model=nullptr;
+};
+#endif // MAINWINDOW_H
